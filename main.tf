40489db11dcc2b1b45bf2a05c62d2fa886ef6fc0
Hardy Pottinger <hardy.pottinger@gmail.com>
2018-06-22 11:39:55 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+ region = "us-west-2"
+}
+
+# create a base Samvera server
+resource "aws_instance" "kickme" {
+  ami = "ami-d07d31a8" #use my kickme/hyrax AMI
+  instance_type = "t2.micro"
+  key_name = "hpottinger"
+  security_groups = ["hardy_sHouse"]
+  tags {
+  Name = "terraform-samvera-hyrax-kickme"
+  }
+}
