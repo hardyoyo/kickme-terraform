23818c320f43d9a7bbe4f88b8b3c8664949bc096
Hardy Pottinger <hardy.pottinger@gmail.com>
2018-06-26 12:58:21 -0500

    t2.micro is too teeny, moving up to t2.small

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # create a base Samvera server
 resource "aws_instance" "kickme" {
   ami = "ami-d07d31a8" #use my kickme/hyrax AMI
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name = "hpottinger"
   security_groups = ["hardy_sHouse"]
   tags {
