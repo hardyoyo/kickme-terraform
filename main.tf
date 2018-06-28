c5d2be1acb9aebbcc1176add10fe712905504a46
Hardy Pottinger <hardy.pottinger@gmail.com>
2018-06-28 12:10:48 -0500

    change security group

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "kickme" {
   ami = "ami-30e6ae48" #use my kickme/hyrax AMI
   instance_type = "c5d.large"
   key_name = "hpottinger"
-  security_groups = ["hardy_sHouse"]
+  security_groups = ["UCLA-VPN-allow"]
   tags {
   Name = "terraform-samvera-hyrax-kickme"
   }
